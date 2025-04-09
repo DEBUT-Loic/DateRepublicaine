43720c0b9e8b8250064480ea565db0fa94aa9ecc
DEBUT Loïc <94994920+DEBUT-Loic@users.noreply.github.com>
2025-04-09 18:44:23 +0200

    Rangement des informations

--- script/conversion.js
+++ script/conversion.js
@@ -1,33 +1,66 @@
-var dateConv, dC;
-console.log('le chien');
-$("#input_date").keyup(function() {
-    if ($(this).val() >= '1792-09-22') {
-        dC=new Date($(this).val());
-        console.log(dC.getMonth());
-        dateConv=dayOfDecadeName(dC)+' '+dayOfMonth(dC)+' '+monthName(dC)+' an '+chiffreRomain(year(dC));
-        console.log(dateConv);
-        
-        $('#conversion span').html('(an '+year(dC)+')')
-        $(".dateC").html(firstLetterUC(dateConv));
-        $(".jourC").html(firstLetterUC(dayOfYearName(dC)));      
+$(document).ready(() => {
+    var date;
 
-        dC.getSeconds()<10 ? sec="0"+dC.getSeconds() : sec=dC.getSeconds();
-        dC.getMinutes()<10 ? min="0"+dC.getMinutes() : min=dC.getMinutes();
-        dC.getHours()<10 ? h="0"+dC.getHours() : h=dC.getHours();
-        horaire=h+" h "+min+" : "+sec;
+    // Mettre la date grégorienne et républicaine
+    function dateAffichage() {
+        date = new Date();
 
-        now=(dC.getSeconds()+dC.getMinutes()*60+dC.getHours()*3600)*(1/0.864);
-        heure=Math.floor(now/(100*100)) % 10
-        minute=Math.floor(now/100) % 100
-        seconde=Math.floor(now) % 100
+        // GRÉGORIENNE
+        // Options pour la date en toutes lettres
+        const options = { weekday: "long", year: "numeric", month: "long", day: "2-digit" };
+        $("#gregorien > .date").text(firstLetterUC(date.toLocaleDateString('fr', options)));
 
-        heure < 10 ? heure="0"+heure : heure=heure
-        minute < 10 ? minute="0"+minute : minute=minute
-        seconde < 10 ? seconde="0"+seconde : seconde=seconde
-        horDec=heure+" h "+minute+" : "+seconde;
-        console.log(horaire)
-        console.log(horDec)
+        // Formatage de l'heure avec padding
+        const h = String(date.getHours()).padStart(2, "0");
+        const min = String(date.getMinutes()).padStart(2, "0");
+        const sec = String(date.getSeconds()).padStart(2, "0");       
+        $("#gregorien > .heure").text(`${h} h ${min} : ${sec}`);
 
-        $(".hC").html(horDec);   
+        // --------------------------------------------------------------------------------------
+
+        // RÉPUBLICAINE
+        let dateRep;
+        switch (date.getTimezoneOffset()) {
+            case -60:
+                date.setHours(date.getHours()-1);
+                dateRep=`${firstLetterUC(dayOfDecadeName(date))} ${dayOfMonth(date)} ${firstLetterUC(monthName(date))} an ${convertToRomanNumeral(year(date))}`;
+                date.setHours(date.getHours()+1);
+                break;
+            
+            default:
+                dateRep=`${firstLetterUC(dayOfDecadeName(date))} ${dayOfMonth(date)} ${firstLetterUC(monthName(date))} an ${convertToRomanNumeral(year(date))}`;
+                break;
+        }
+
+        const tempsDecimal=(date.getSeconds()+date.getMinutes()*60+date.getHours()*3600) / 0.864;
+        const heure=Math.floor(tempsDecimal/10_000) % 10 // 1 heure = 100 secondes * 100 minutes
+        const minute=Math.floor(tempsDecimal/100) % 100 // 1 minute = 100 secondes
+        const seconde=Math.floor(tempsDecimal) % 100
+
+        const pad = (n) => String(n).padStart(2, "0");
+        const horDec=`${pad(heure)} h ${pad(minute)} : ${pad(seconde)}`;
+
+        $("#republic > .date").text(dateRep);
+        $("#republic > .heure").text(horDec);
+        $("#gregorien h1 > span").text(`(an ${year(date)})`)
+
+        // Mettre le fond parchemin
+        if(month(date) < 4) {
+            $('body').css('background-image','url("img/parchemin automne.png")');
+        }
+        else if(month(date) < 7) {
+            $('body').css('background-image','url("img/parchemin hiver.png")');
+        }
+        else if(month(date) < 10) {
+            $('body').css('background-image','url("img/parchemin printemps.png")');
+        }
+        else if(month(date) < 13) {
+            $('body').css('background-image','url("img/parchemin été.png")');
+        }
+        else {
+            $('body').css('background-image','url("img/parchemin.jpg")');
+        }
     }
+    dateAffichage();
+    setInterval(dateAffichage,1000);
 })
